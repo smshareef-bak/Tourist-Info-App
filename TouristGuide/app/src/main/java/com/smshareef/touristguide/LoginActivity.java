78e3f24684a3df08e8bca272fee9b8cd4d5ae8bf
Mahboob Shareef Shaik <smshareef@outlook.com>
2017-05-10 00:15:57 +0530

    Initial login screen draft

--- TouristGuide/app/src/main/java/com/smshareef/touristguide/LoginActivity.java
+++ TouristGuide/app/src/main/java/com/smshareef/touristguide/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.smshareef.touristguide;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView tv_login_welcome = (TextView) findViewById(R.id.tv_login_welcome);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/NEOTERICc - Regular.ttf");
+        tv_login_welcome.setTypeface(typeface);
     }
 }
